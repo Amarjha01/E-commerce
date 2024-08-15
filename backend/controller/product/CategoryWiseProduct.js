0b8a126bc14ba6166ed4ac92dbeb0ae35bf63434
Amar <amarjha880@gmail.com>
2024-08-15 18:01:48 +0530

    new Updates

--- backend/controller/product/CategoryWiseProduct.js
+++ backend/controller/product/CategoryWiseProduct.js
@@ -2,16 +2,13 @@ import productModel from "../../models/productModel.js";
 
 const CategoryWiseProduct = async (req, res) => {
   try {
-    const { category } = req?.body || req?.query;
+    const { category } = req?.body || req.query;
+
     const product = await productModel.find({ category });
+
     res.json({
       data: product,
-      message: "product category",
-      sucess:true,
-      error:false
     });
-
-    console.log(product);
   } catch (error) {
     res.status(500).json({
       message: error.message,
